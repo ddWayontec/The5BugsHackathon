74be86c582fa9ee905ef78e5aeea6c0e2756023f
karynnglli <karynngl.li@ucalgary.ca>
2018-02-18 11:50:15 -0700

    All WebHooks

--- mainFinal/systemWithWebhookAndPhoneNumberChangerAndBiWeb.ino
+++ mainFinal/systemWithWebhookAndPhoneNumberChangerAndBiWeb.ino
@@ -56,7 +56,6 @@ void setup()
 
 void loop()
 {
-
   if(setMotionSMS == 0)
   {
     Serial.println("Sending disarmed SMS...");
@@ -107,8 +106,6 @@ void loop()
 
     float R0 = MQCalibration();
     printGasValues(R0);
-
-
 }
 
 int callback(int type, const char* buf, int len, char* param)
@@ -281,15 +278,19 @@ void printGasValues(float r0Value) {
   Serial.println("======");
   Serial.print("Air Purity (R0) [higher # = cleaner air]: ");
   Serial.println(r0Value);
+  Particle.publish("Gas Sensor Readings", r0Value, PRIVATE);
   Serial.print("Liquified Petroleum Gas (LPG): ");
   Serial.print(getGasPercentage(MQRead()/r0Value, LPG_GAS) );
+  Particle.publish("Gas Sensor Readings", LPG_GAS, PRIVATE);
   Serial.println( " ppm" );
   Serial.print("Carbon Monoxide (CO): ");
   Serial.print(getGasPercentage(MQRead()/r0Value, CO_GAS) );
+  Particle.publish("Gas Sensor Readings", CO_GAS, PRIVATE);
   Serial.println( " ppm" );
   Serial.print("SMOKE: ");
   Serial.print(getGasPercentage(MQRead()/r0Value, SMOKE_GAS) );
+  Particle.publish("Gas Sensor Readings", data, PRIVATE);
   Serial.println( " ppm" );
   Serial.println();
-  delay(200);
+//  delay(200);
 }

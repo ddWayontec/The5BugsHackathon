4949b99a31ffcdb1fc3dc460014f0f22acc0650d
Rakheem Dewji <rakheemdewji@hotmail.com>
2018-02-17 17:26:37 -0700

    added a test to see if sensor is working

---
+++ detecterTest.ino
@@ -0,0 +1,28 @@
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+    float sensor_volt;
+    float gas_ratio;
+    float RS_air;
+    float sensorValue;
+    float R0;
+
+
+    for (int i = 0; i < 100; i++){
+      sensorValue = sensorValue + analogRead(A0);
+    }
+
+    sensorValue = sensorValue/100.0;
+    sensor_volt = (float)sensorVal/1024*5.0;
+    RS_air = (5.0-sensor_volt)/sensor_volt;
+    R0 = RS_air/10.0                          // ratio is always 10 in clear air
+
+
+    Serial.print("Sensor value= ")
+    Serial.print(sensorValue);
+    Serial.println("R0 = ");
+    Serial.print(RO);
+
+}
